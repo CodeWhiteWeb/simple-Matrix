630a0a8370c56928708a8280223db0447ca6c61a
CodeWhiteWeb <79645854+CodeWhiteWeb@users.noreply.github.com>
2022-06-23 18:55:08 +0530

    lol

---
+++ matrix.bat
@@ -0,0 +1,8 @@
+@echo off 
+pause
+color 0a
+title Matrix effect
+mode 1000
+:top
+echo %random%  %random%  %random%  %random% %random%  %random%  %random%  %random% %random%  %random%  %random%  %random% %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random% %random%  %random%  %random%  %random%  %random%  %random%  %random%
+goto top
